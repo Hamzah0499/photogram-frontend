c45f55d1220eba4d454fff706966f1e1def527d9
Hamzah0499 <Hamzah-M@ulster.ac.uk>
2026-01-12 05:36:08 +0500

    Updated Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,19 @@
 export default function Avatar({
   src,
-}:{
-  src:string;
+}: {
+  src: string;
 }) {
   return (
     <div className="size-16 aspect-square overflow-hidden rounded-full">
-      <img
-        src={src}
-        alt=""/>
+      {src ? (
+        <img
+          src={src}
+          alt="" />
+      ) : (
+        <p className='h-4 w-4 rounded-full bg-gray-500'>
+          Nil
+        </p>
+      )}
     </div>
   );
 }
